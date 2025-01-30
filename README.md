5f2d0559d50f815be73e6f18947bc01b5c22aa9d
Mikko Keinänen <mikko.keinanen@gmail.com>
2025-01-30 22:44:39 +0200

    rust setup

---
+++ README.md
@@ -0,0 +1,28 @@
+# Global
+
+lead key: \
+
+## shortcuts with lead:
+
+n   Toggle NERDTree
+
+# Rust-specific
+
+K   hover actions
+
+## shortcuts with lead:
+
+<leader>a   code actions
+
+# diagnostics
+
+<leader>d   show diagnostics
+[d          jump to previous diagnostics
+]d          jump to next diagnostics
+
+## other
+
+gd  go to definition
+gi  go to implementation
+gr  find references
+gT  go to type definition
--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,7 +1,5 @@
 local bufnr = vim.api.nvim_get_current_buf()
 
-print("Rust-specific configuration loaded!")
-
 -- show code actions in a floating window
 vim.keymap.set("n", "<leader>a", 
   function()
@@ -18,4 +16,19 @@ vim.keymap.set("n", "K",  -- Override Neovim's built-in hover keymap with rustac
   { silent = true, buffer = bufnr }
 )
 
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { buffer = true })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { buffer = true })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { buffer = true })
+
+-- Navigate to definition
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = true, silent = true })
+
+-- Navigate to implementation
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = true, silent = true })
+
+-- Show references
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = true, silent = true })
+
+-- Go to type definition
+vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = true, silent = true })
 
